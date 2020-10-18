d8ec12079e0a297d85f64badb2f3c41cffba240f
Victor Escalona <victorescalonag@gmail.com>
2020-10-18 17:49:24 -0500

    Removed a test view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { Switch, Route, Redirect, useHistory } from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 import Home from "./views/Home/Home";
 import Login from "./views/Login/Login";
-import FormValidator from "./components/FormValidator/FormValidator";
 import NotFound from "./views/NotFound/NotFound";
 
 const App = () => {
@@ -31,7 +30,6 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/home" component={Home} />
-        <Route exact path="/test" component={FormValidator} />
         <Route path="*" component={NotFound} />
       </Switch>
     </>
