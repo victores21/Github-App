1783f0a97a18d56da17e28e218d19a2ab0ceaf54
Victor Escalona <victorescalonag@gmail.com>
2020-10-18 03:02:42 -0500

    Now home is showing information from the API

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -7,7 +7,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import "./Home.css";
 
 const Home = () => {
-  const [userInfo, setUserInfo] = useState({
+  const [cookieInfo, setCookieInfo] = useState({
     username: "",
     email: "",
     firstName: "",
@@ -18,14 +18,45 @@ const Home = () => {
     birthDate: "",
   });
 
-  useEffect(() => {
-    // const getData = async () => {
-    //   const req = await fetch("https://api.github.com/users/victores21");
-    //   const data = await req.json();
-    //   console.log(await data);
-    // };
+  const [userInfo, setUserInfo] = useState({
+    avatar_url: "",
+    name: "",
+    username: "",
+    company: "",
+    location: "",
+    created_at: "",
+    public_repos: "",
+    followers: "",
+    following: "",
+  });
+  const [loading, setLoading] = useState(true);
 
-    // getData();
+  useEffect(() => {
+    const getUserInfo = async (username) => {
+      try {
+        const req = await fetch(`https://api.github.com/users/${username}`);
+        const data = await req.json();
+        console.log(await data);
+        setUserInfo({
+          avatar_url: data.avatar_url,
+          name: data.name,
+          username: data.login,
+          company: data.company,
+          location: data.location,
+          created_at: data.created_at,
+          public_repos: data.public_repos,
+          followers: data.followers,
+          following: data.following,
+        });
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUserInfo(cookieInfo.username);
+  }, [cookieInfo.username]);
+
+  useEffect(() => {
     const getCookie = () => {
       let cookies = document.cookie
         .split(";")
@@ -38,7 +69,7 @@ const Home = () => {
           {}
         );
 
-      setUserInfo({
+      setCookieInfo({
         username: cookies.username,
         email: cookies.email,
         firstName: cookies.firstName,
@@ -49,86 +80,99 @@ const Home = () => {
         birthDate: cookies.birthDate,
       });
     };
+
     getCookie();
   }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <>
+        <Navbar cookieInfo={cookieInfo} />
 
-  return (
-    <>
-      <Navbar userInfoFromCookies={userInfo} />
-
-      <div className="hero-home">
-        <div className="hero-content-container">
-          {/* <!-- Top content container --> */}
-          <div className="hero-top-container">
-            <div className="hero-top-content-container">
-              <div className="hero-image-container">
-                <img className="hero-image" src="/images/perfil.png" alt="" />
-              </div>
-
-              <div className="hero-user-content-container">
-                <h1 id="hero-name">Victor Escalona</h1>
-                <h2 id="hero-username">@{userInfo.username}</h2>
-
-                <div className="hero-personal-information">
-                  <p className="personal-info work-place">
-                    <i className="fas fa-briefcase"></i> Ra Marketing Consulting
-                  </p>
-                  <p className="personal-info place-living">
-                    <i className="fas fa-map-marker-alt"></i> Bucaramanga,
-                    Colombia
-                  </p>
-                  <p className="personal-info github-created">
-                    <i className="far fa-calendar"></i> Joined in, February, 13,
-                    2018
-                  </p>
+        <div className="hero-home">
+          <div className="hero-content-container">
+            {/* <!-- Top content container --> */}
+            <div className="hero-top-container">
+              <div className="hero-top-content-container">
+                <div className="hero-image-container">
+                  <img
+                    className="hero-image"
+                    src={userInfo.avatar_url}
+                    alt=""
+                  />
                 </div>
 
-                <div className="hero-information-cards">
-                  <div className="hero-information-card">
-                    <h3 className="hero-information-card-number">18</h3>
-                    <h4 className="hero-information-card-category">
-                      Repositorios
-                    </h4>
+                <div className="hero-user-content-container">
+                  <h1 id="hero-name">{userInfo.name}</h1>
+                  <h2 id="hero-username">@{userInfo.username}</h2>
+
+                  <div className="hero-personal-information">
+                    <p className="personal-info work-place">
+                      <i className="fas fa-briefcase"></i> {userInfo.company}
+                    </p>
+                    <p className="personal-info place-living">
+                      <i className="fas fa-map-marker-alt"></i>{" "}
+                      {userInfo.location}
+                    </p>
+                    <p className="personal-info github-created">
+                      <i className="far fa-calendar"></i> {userInfo.created_at}
+                    </p>
                   </div>
-                  <div className="hero-information-card">
-                    <h3 className="hero-information-card-number">4</h3>
-                    <h4 className="hero-information-card-category">
-                      Seguidores
-                    </h4>
-                  </div>
-                  <div className="hero-information-card">
-                    <h3 className="hero-information-card-number">4</h3>
-                    <h4 className="hero-information-card-category">
-                      Siguiendo
-                    </h4>
+
+                  <div className="hero-information-cards">
+                    <div className="hero-information-card">
+                      <h3 className="hero-information-card-number">
+                        {userInfo.public_repos}
+                      </h3>
+                      <h4 className="hero-information-card-category">
+                        Repositorios
+                      </h4>
+                    </div>
+                    <div className="hero-information-card">
+                      <h3 className="hero-information-card-number">
+                        {userInfo.followers}
+                      </h3>
+                      <h4 className="hero-information-card-category">
+                        Seguidores
+                      </h4>
+                    </div>
+                    <div className="hero-information-card">
+                      <h3 className="hero-information-card-number">
+                        {userInfo.following}
+                      </h3>
+                      <h4 className="hero-information-card-category">
+                        Siguiendo
+                      </h4>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          {/* <!-- Bottom content container --> */}
-          <div className="hero-bottom-container">
-            <div className="hero-bottom-content-container">
-              <h2 id="hero-bottom-title">Repositorios</h2>
+            {/* <!-- Bottom content container --> */}
+            <div className="hero-bottom-container">
+              <div className="hero-bottom-content-container">
+                <h2 id="hero-bottom-title">Repositorios</h2>
 
-              <div className="hero-bottom-search-container">
-                <input
-                  type="text"
-                  name=""
-                  id="hero-bottom-search"
-                  placeholder="Buscar Repositorio"
-                />
-              </div>
-              <div className="hero-bottom-table">
-                {/* <!-- Here goes the table with re repositories --> */}
+                <div className="hero-bottom-search-container">
+                  <input
+                    type="text"
+                    name=""
+                    id="hero-bottom-search"
+                    placeholder="Buscar Repositorio"
+                  />
+                </div>
+                <div className="hero-bottom-table">
+                  {/* <!-- Here goes the table with re repositories --> */}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Home;
