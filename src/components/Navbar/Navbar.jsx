d7e5712db38a4bfc956108fe5f7b758ef24b2437
Victor Escalona <victorescalonag@gmail.com>
2020-10-18 02:31:45 -0500

    Now the Navbar is using the information that Home es receiving from login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,22 +3,34 @@ import React from "react";
 //Style
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const {
+    username,
+    email,
+    firstName,
+    secondName,
+    lastName,
+    secondLastName,
+    ci,
+    birthDate,
+  } = props.userInfoFromCookies;
   return (
     <>
       <header className="header">
         <div className="header-content-container">
           <input className="menu-btn" type="checkbox" id="menu-btn" />
-          <label className="menu-icon" for="menu-btn">
+          <label className="menu-icon" htmlFor="menu-btn">
             <span className="navicon"></span>
           </label>
           <ul className="menu">
             <li>Bienvenido</li>
-            <li>Victor David Escalona Gonzalez</li>
-            <li>Usuario: @Victores21</li>
-            <li>Correo: victorescalonag@gmail.com</li>
-            <li>Cedula: 1095845082</li>
-            <li>Fecha de nacimiento: 02-02-1999</li>
+            <li>
+              {firstName} {secondName} {lastName} {secondLastName}
+            </li>
+            <li>Usuario: @{username}</li>
+            <li>Correo: {email}</li>
+            <li>Cedula: {ci}</li>
+            <li>Fecha de nacimiento: {birthDate}</li>
           </ul>
         </div>
       </header>
