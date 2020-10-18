f3b016d4e9ddc3d1f7b7f7e7204064d51802af2c
Victor Escalona <victorescalonag@gmail.com>
2020-10-18 10:07:23 -0500

    Added a new function to add the cookies

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 //Styles
 import "./Login.css";
 
 const Login = () => {
+  const [formValue, setFormValue] = useState({
+    username: "",
+    email: "",
+    firstName: "",
+    secondName: "",
+    lastName: "",
+    secondLastName: "",
+    ci: "",
+    birthDate: "",
+  });
+
+  const handleEmailChange = (e) => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const addCookies = () => {
+    const {
+      username,
+      email,
+      firstName,
+      secondName,
+      lastName,
+      secondLastName,
+      ci,
+      birthDate,
+    } = formValue;
+
+    document.cookie = `username= ${username}`;
+    document.cookie = `email= ${email}`;
+    document.cookie = `firstName= ${firstName}`;
+    document.cookie = `secondName= ${secondName}`;
+    document.cookie = `lastName= ${lastName}`;
+    document.cookie = `secondLastName= ${secondLastName}`;
+    document.cookie = `ci= ${ci}`;
+    document.cookie = `birthDate= ${birthDate}`;
+  };
+
   return (
     <>
       <div className="hero-login">
@@ -15,17 +56,23 @@ const Login = () => {
           <div className="hero-form">
             <h1 id="hero-form-title">Encuentra tu perfil de Github</h1>
 
-            <form className="form-container" action="#">
+            <form
+              className="form-container"
+              action="#"
+              onChange={(e) => handleEmailChange(e)}
+            >
               <div className="form-content">
                 <input
                   className="form-content-input"
                   type="text"
                   placeholder="Usuario de Github"
+                  name="username"
                 />
                 <input
                   className="form-content-input"
                   type="email"
                   placeholder="Correo Electrónico"
+                  name="email"
                 />
                 <div className="shared-container-input">
                   <input
@@ -33,12 +80,14 @@ const Login = () => {
                     type="text"
                     id="primer-nombre"
                     placeholder="Primer Nombre"
+                    name="firstName"
                   />
                   <input
                     className="form-content-input"
                     type="text"
                     id="segundo-nombre"
                     placeholder="Segundo Nombre"
+                    name="secondName"
                   />
                 </div>
 
@@ -47,34 +96,39 @@ const Login = () => {
                     className="form-content-input"
                     type="text"
                     placeholder="Primer Apellido"
+                    name="lastName"
                   />
                   <input
                     className="form-content-input"
                     type="text"
                     placeholder="Segundo Apellido"
+                    name="secondLastName"
                   />
                 </div>
                 <input
                   className="form-content-input"
                   type="number"
                   placeholder="Cedula"
+                  name="ci"
                 />
-                <label for="fecha-nacimiento" id="fecha-nacimiento-label">
-                  Fecha de Nacimiento
-                </label>
+                <label id="fecha-nacimiento-label">Fecha de Nacimiento</label>
                 <input
                   className="form-content-input"
                   type="date"
                   id="fecha-nacimiento"
                   placeholder="Fecha de Nacimiento"
+                  name="birthDate"
                 />
               </div>
               <div className="submit-button-container">
-                <input
-                  className="btn btn-transparent btn-white-outline color-white btn-entrar"
-                  type="submit"
-                  value="Entrar"
-                />
+                <Link to="/home">
+                  <input
+                    className="btn btn-transparent btn-white-outline color-white btn-entrar"
+                    type="submit"
+                    value="Entrar"
+                    onClick={() => addCookies()}
+                  />
+                </Link>
               </div>
             </form>
           </div>
